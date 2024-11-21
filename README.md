b6b857a6df6d5f2c0f6676a08f76ceb89e64a84c
patricklafrance <pa.lafrance@gmail.com>
2024-11-20 23:17:17 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# react-table-header-interface-issue
+
+To reproduce the issue:
+
+- `pnpm install`
+- `pnpm build`
---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+// WORKS:
+
+// import { Column, TableHeader, type TableHeaderProps } from "react-aria-components";
+
+// export interface MyTableHeaderProps extends TableHeaderProps<object> {
+//     foo: string;
+// }
+
+// export function MyTableHeader({ foo, ...props }: MyTableHeaderProps) {
+//     return (
+//         <TableHeader {...props}>
+//             <Column>{foo}</Column>
+//         </TableHeader>
+//     )
+// }
+
+// DOESN'T WORKS:
+
+import { THead as OrbiterTableHeader, as } from "@workleap/orbiter-ui";
+import type { ComponentProps } from "react";
+import { TableHeader as ReactAriaTableHeader } from "react-aria-components";
+
+/** This component is a wrapper to allow the use of CSS in JS on React Aria's component */
+const InnerTableHeader = as(OrbiterTableHeader, ReactAriaTableHeader);
+
+export type TableHeaderProps = ComponentProps<typeof InnerTableHeader>;
+
+export const TableHeader = ({ children, ...rest }: TableHeaderProps) => (
+    <InnerTableHeader textAlign="left" height="3rem" {...rest}>
+        {children}
+    </InnerTableHeader>
+);
